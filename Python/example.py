fdb964107e25d9bbe6902a0b9205675583362fe3
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-10-20 09:50:55 +0200

    Creada carpeta con programa para crear xml

---
+++ Python/example.py
@@ -0,0 +1,5 @@
+from grobid_client.grobid_client import GrobidClient
+
+if __name__ == "__main__":
+    client = GrobidClient(config_path="./config.json")
+    client.process("processFulltextDocument", "./../Articulos", output="./../TEI",  force=True, verbose=True)
