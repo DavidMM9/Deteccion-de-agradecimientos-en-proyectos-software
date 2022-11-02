fda1f9f7b4ef558fe0b08c83296b87882e401268
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-11-02 17:30:37 +0100

    Añadido path para los json esperados

--- Python/Json_reader.py
+++ Python/Json_reader.py
@@ -1,19 +1,24 @@
 import json
 import os
 
+path = "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste/"
+compared = "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/TEI/Json/"
 allfiles = []
-for filename in os.listdir("/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste"):
+
+for filename in os.listdir(path):
     allfiles.append(filename)
 
 # print(allfiles)
 
 for k in allfiles:
     # Abrimos el json
-    f = open(
-        "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste/" + k)
+    f = open(path + k)
     data = json.load(f)
+    c = open(compared + k)
+    dataCompared = json.load(c)
 
     print("Estoy en " + k)
+
     for i in data:
         # i['word'] = lo que ha reconocido
         # i['entity_group'] = que ha dicho que es
@@ -21,3 +26,4 @@ for k in allfiles:
 
     # Cerramos el json
     f.close()
+    c.close()
