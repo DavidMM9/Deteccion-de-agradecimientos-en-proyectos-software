33a276981196c379a084058b683e663392066d25
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-11-02 18:51:01 +0100

    Terminado programa automatico de comparacion

--- Python/Json_reader.py
+++ Python/Json_reader.py
@@ -1,16 +1,20 @@
 import json
 import os
 
+# De donde cojo los json para probar
 path = "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste/"
+# Donde estan los json buenos
 compared = "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/TEI/Json/"
 allfiles = []
 res = 0
+total = 0
+total2 = 0
 
+# Meto todos los archivos de la carpeta en una variable
 for filename in os.listdir(path):
     allfiles.append(filename)
 
-# print(allfiles)
-
+# Hago un bucle para abrir de uno en uno los archivos
 for k in allfiles:
     # Abrimos el json
     f = open(path + k)
@@ -18,18 +22,26 @@ for k in allfiles:
     c = open(compared + k)
     dataCompared = json.load(c)
 
-    # print("Estoy en " + k)
-
+    # Bucle para cada uno de los elementos del archivo
     for i in data:
+        # Bucle para comparar los elementos de los dos archivos
         for j in dataCompared:
             # i['word'] = lo que ha reconocido
             # i['entity_group'] = que ha dicho que es
-            # print("Ha reconocido " + i['word'] + " como " + i['entity_group'])
-            # print(j['word'])
-            if ((i['word'] == j['word']) and (i['entity_group'] == j['entity_group'])):
-                res = res + 1
+            # Compruebo que tanto el entity_group como el word sean iguales para comprobar
+            # si el modelo ha detectado bien el elemento
+            if ((i['word'] == j['word'])):
+                if ((i['entity_group'] == j['entity_group'])):
+                    res = res + 1
+
+    # Voy añadiendo cuantos elementos hay en cada archivo para obtener el total
+    total = total + len(data)
+    total2 = total2 + len(dataCompared)
 
     # Cerramos el json
     f.close()
     c.close()
+
+print(total)
+print(total2)
 print("De los resultados obtenidos " + str(res) + " estan bien")
