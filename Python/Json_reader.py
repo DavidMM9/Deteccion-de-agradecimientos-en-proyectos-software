a296170f982fe78dc62278d2104ba0365a81c930
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-11-02 17:43:39 +0100

    Actualizada comparacion

--- Python/Json_reader.py
+++ Python/Json_reader.py
@@ -4,6 +4,7 @@ import os
 path = "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste/"
 compared = "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/TEI/Json/"
 allfiles = []
+res = 0
 
 for filename in os.listdir(path):
     allfiles.append(filename)
@@ -17,13 +18,18 @@ for k in allfiles:
     c = open(compared + k)
     dataCompared = json.load(c)
 
-    print("Estoy en " + k)
+    # print("Estoy en " + k)
 
     for i in data:
-        # i['word'] = lo que ha reconocido
-        # i['entity_group'] = que ha dicho que es
-        print("Ha reconocido " + i['word'] + " como " + i['entity_group'])
+        for j in dataCompared:
+            # i['word'] = lo que ha reconocido
+            # i['entity_group'] = que ha dicho que es
+            # print("Ha reconocido " + i['word'] + " como " + i['entity_group'])
+            # print(j['word'])
+            if ((i['word'] == j['word']) and (i['entity_group'] == j['entity_group'])):
+                res = res + 1
 
     # Cerramos el json
     f.close()
     c.close()
+print("De los resultados obtenidos " + str(res) + " estan bien")
