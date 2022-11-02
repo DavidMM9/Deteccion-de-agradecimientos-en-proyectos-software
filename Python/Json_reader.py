127f69048b55535ab1e60d6760c872c0c74c3ce3
DavidMM9 <david.mmatamala@alumnos.upm.es>
2022-11-02 16:53:37 +0100

    Leo y separo de todos los json de la carpeta

--- Python/Json_reader.py
+++ Python/Json_reader.py
@@ -1,11 +1,23 @@
 import json
+import os
 
-f = open(
-    "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste/Renewable_energy_in_Europe-2020.json"
-)
-data = json.load(f)
+allfiles = []
+for filename in os.listdir("/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste"):
+    allfiles.append(filename)
 
-for i in data:
-    print(i)
+# print(allfiles)
 
-f.close()
+for k in allfiles:
+    # Abrimos el json
+    f = open(
+        "/mnt/c/Users/david/Desktop/TFG/Deteccion-de-agradecimientos-en-proyectos-software/HuggingFace/Jean-Baptiste/" + k)
+    data = json.load(f)
+
+    print("Estoy en " + k)
+    for i in data:
+        # i['word'] = lo que ha reconocido
+        # i['entity_group'] = que ha dicho que es
+        print("Ha reconocido " + i['word'] + " como " + i['entity_group'])
+
+    # Cerramos el json
+    f.close()
